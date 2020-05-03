d0f59bee7aed6820bbc9cee4b14bbd5d86755be5
niroshan123 <niroshanratnayake07@gmail.com>
2020-05-03 21:44:28 +0530

    setting up

---
+++ Ng-SpringBoot-redit-app-BackEnd/src/main/java/uom/niroshan/myreditapp/MyreditappApplication.java
@@ -0,0 +1,13 @@
+package uom.niroshan.myreditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyreditappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyreditappApplication.class, args);
+	}
+
+}
---
+++ Ng-SpringBoot-redit-app-BackEnd/src/test/java/uom/niroshan/myreditapp/MyreditappApplicationTests.java
@@ -0,0 +1,13 @@
+package uom.niroshan.myreditapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyreditappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
