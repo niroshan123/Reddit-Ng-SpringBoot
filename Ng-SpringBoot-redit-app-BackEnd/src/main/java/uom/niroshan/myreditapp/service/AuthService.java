1133167ae6cca93616f0f932c75a7cba8972e00b
niroshan123 <niroshanratnayake07@gmail.com>
2020-05-17 23:51:13 +0530

    User Authentication testing

--- Ng-SpringBoot-redit-app-BackEnd/src/main/java/uom/niroshan/myreditapp/service/AuthService.java
+++ Ng-SpringBoot-redit-app-BackEnd/src/main/java/uom/niroshan/myreditapp/service/AuthService.java
@@ -1,20 +1,52 @@
 package uom.niroshan.myreditapp.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uom.niroshan.myreditapp.dto.RegisterRequest;
 import uom.niroshan.myreditapp.model.User;
+import uom.niroshan.myreditapp.model.VerificationToken;
+import uom.niroshan.myreditapp.repository.UserRepository;
 
 import java.time.Instant;
+import java.util.UUID;
+
+
+@AllArgsConstructor
 
 @Service
 public class AuthService {
 
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final   UserRepository userRepository;
+
+    @Transactional
     public void signup(RegisterRequest registerRequest){
         User user =new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+        userRepository.save(user);
+
+        generateVerificationToken(user);
+
+    }
+
+    public void generateVerificationToken(User user){
+        String token =UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+
     }
 }
